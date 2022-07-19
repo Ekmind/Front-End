304603b45eb949bfcf6470b2c87d198e9032ba3d
Efrain Estrada <a325060@uach.mx>
2022-07-19 09:20:03 -0600

    Revert "tabla de pacientes"
    This reverts commit f98b2a6c8fb267378c43e41afcfa92acd3a4533d.

--- src/app/componentes/expedient/expedient.component.ts
+++ src/app/componentes/expedient/expedient.component.ts
@@ -1,60 +1,75 @@
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
 import { Doctor } from 'src/app/interfaces/doctor.interface';
 import { AuthService } from 'src/services/authe/auth.service';
-import { threadId } from 'worker_threads';
 
 @Component({
   selector: 'app-expedient',
   templateUrl: './expedient.component.html',
-  styleUrls: ['./expedient.component.css'],
+  styleUrls: ['./expedient.component.css']
 })
 export class ExpedientComponent implements OnInit {
-  user: Doctor | any;
 
-  listPatients: any[] = [
-    {
-      name: 'Efra',
-      lastName: 'Estrada',
-      age: '22',
-      gender: 'male',
-      emile: 'lol@gmail.com',
-    },
-    {
-      name: 'Efra',
-      lastName: 'Estrada',
-      age: '22',
-      gender: 'male',
-      emile: 'lol@gmail.com',
-    },
-  ];
+  user:Doctor|any;
 
-  form: FormGroup;
-
-  constructor(private auth: AuthService, private fb:FormBuilder) {
-    this.form = this.fb.group({
-      name: [''],
-      lastName: [''],
-      age: [''],
-      gender: [''],
-      emile: [''],
-    });
-  }
+  constructor(private auth:AuthService) { }
 
   ngOnInit(): void {
-    this.user = this.auth.user;
-  }
-  addPatient() {
-    console.log(this.form);
-
-    const patient: any = {
-      name: this.form.get('name')?.value,
-      lastName: this.form.get('lastName')?.value,
-      age: this.form.get('age')?.value,
-      gender: this.form.get('gender')?.value,
-      emile: this.form.get('emile')?.value,
-    };
+    this.user = this.auth.user
   }
 }
 
 
+/* <script>
+  $(document).ready(function () {
+    $('[data-toggle="tooltip"]').tooltip();
+    var actions = $("table td:last-child").html();
+    // Append table with add row form on add new button click
+    $(".add-new").click(function () {
+      $(this).attr("disabled", "disabled");
+      var index = $("table tbody tr:last-child").index();
+      var row = '<tr>' +
+        '<td><input type="text" class="form-control" name="name" id="name"></td>' +
+        '<td><input type="text" class="form-control" name="department" id="department"></td>' +
+        '<td><input type="text" class="form-control" name="phone" id="phone"></td>' +
+        '<td>' + actions + '</td>' +
+        '</tr>';
+      $("table").append(row);
+      $("table tbody tr").eq(index + 1).find(".add, .edit").toggle();
+      $('[data-toggle="tooltip"]').tooltip();
+    });
+    // Add row on add button click
+    $(document).on("click", ".add", function () {
+      var empty = false;
+      var input = $(this).parents("tr").find('input[type="text"]');
+      input.each(function () {
+        if (!$(this).val()) {
+          $(this).addClass("error");
+          empty = true;
+        } else {
+          $(this).removeClass("error");
+        }
+      });
+      $(this).parents("tr").find(".error").first().focus();
+      if (!empty) {
+        input.each(function () {
+          $(this).parent("td").html($(this).val());
+        });
+        $(this).parents("tr").find(".add, .edit").toggle();
+        $(".add-new").removeAttr("disabled");
+      }
+    });
+    // Edit row on edit button click
+    $(document).on("click", ".edit", function () {
+      $(this).parents("tr").find("td:not(:last-child)").each(function () {
+        $(this).html('<input type="text" class="form-control" value="' + $(this).text() + '">');
+      });
+      $(this).parents("tr").find(".add, .edit").toggle();
+      $(".add-new").attr("disabled", "disabled");
+    });
+    // Delete row on delete button click
+    $(document).on("click", ".delete", function () {
+      $(this).parents("tr").remove();
+      $(".add-new").removeAttr("disabled");
+    });
+  });
+</script> */

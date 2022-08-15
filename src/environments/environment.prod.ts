332b1b337a2bcca9f28837e0db11c5dc335cdaf5
Carlos Rodrigo Acosta Domínguez <71846585+Saf-ire@users.noreply.github.com>
2022-08-15 10:30:31 -0600

    --prod deprecated

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   production: true,
-  title:"Heroku enviroment",
-  apiURL:"https://ekmind-api.herokuapp.com/"
+  title: 'Heroku enviroment',
+  apiURL: 'https://ekmind-api.herokuapp.com/',
+  localURL: 'http://localhost:4000/',
 };
